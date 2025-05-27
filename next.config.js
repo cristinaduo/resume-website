92bc3e4a2357faa8496b53b2a7618fb28643827a
duoduo666 <115779083+cristinaduo@users.noreply.github.com>
2025-05-27 23:01:32 +0800

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  images: {
-    unoptimized: true
-  },
-  typescript: {
-    ignoreBuildErrors: true
-  },
-  eslint: {
-    ignoreDuringBuilds: true
-  }
-}
+   /** @type {import('next').NextConfig} */
+   const nextConfig = {
+     output: 'standalone',
+     images: {
+       unoptimized: true
+     },
+     typescript: {
+       ignoreBuildErrors: true
+     },
+     eslint: {
+       ignoreDuringBuilds: true
+     },
+     experimental: {
+       optimizeCss: true
+     },
+     webpack: (config) => {
+       config.resolve.fallback = { fs: false, path: false };
+       return config;
+     }
+   }
 
-module.exports = nextConfig 
+   module.exports = nextConfig
